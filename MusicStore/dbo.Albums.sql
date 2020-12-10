cb894a7116f3b9f95e0ddbebd4fb114bbd51838d
Ergina.R <Ergina.Resulaj@fti.edu.al>
2020-12-10 16:08:19 +0100

    Version 1

---
+++ MusicStore/dbo.Albums.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Albums] (
+    [AlbumID] INT             IDENTITY (1, 1) NOT NULL,
+    [Genre]   NVARCHAR (50)   NOT NULL,
+    [Title]   NVARCHAR (50)   NOT NULL,
+    [Artist]  NVARCHAR (50)   NOT NULL,
+    [Price]   DECIMAL (16, 2) NOT NULL,
+    PRIMARY KEY CLUSTERED ([AlbumID] ASC)
+);
+
